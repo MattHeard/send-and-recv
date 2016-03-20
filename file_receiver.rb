afb5d4802764c48780d3865766bc49469055b687
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:39:53 +1300

    Add out_file to FileReceiver

--- file_receiver.rb
+++ file_receiver.rb
@@ -1,2 +1,11 @@
 class FileReceiver
+  attr_reader :out_file
+
+  def initialize(args)
+    @out_file = args[:out_file]
+  end
+
+  def call
+    out_file.print("abc")
+  end
 end
--- spec/file_receiver_spec.rb
+++ spec/file_receiver_spec.rb
@@ -1,4 +1,21 @@
 require_relative "../file_receiver.rb"
 
 RSpec.describe FileReceiver do
+  let(:file_receiver) { FileReceiver.new(file_receiver_args) }
+  let(:file_receiver_args) do
+    { out_file: out_file, in_stream: in_stream, out_stream: out_stream }
+  end
+  let(:out_stream) { StringIO.new }
+  let(:in_stream) { StringIO.new(in_stream_contents) }
+  let(:out_file) { StringIO.new }
+
+  context "receiving 'abc'" do
+    let(:in_stream_contents) { "abc" }
+
+    before { file_receiver.call }
+
+    it "puts 'abc' in the output file" do
+      expect(out_file.string).to eq in_stream_contents
+    end
+  end
 end
