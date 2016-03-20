c8a21c2154ee20d7397528e647008affd143a6ed
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:25:36 +1300

    Add in_file to FileSender

--- file_sender.rb
+++ file_sender.rb
@@ -1,11 +1,14 @@
 class FileSender
-  attr_reader :out_stream
+  attr_reader :in_file, :out_stream
 
   def initialize(args)
+    @in_file = args[:in_file]
     @out_stream = args[:out_stream]
   end
 
   def call
-    out_stream.print("abc")
+    in_file_contents = in_file.string
+
+    out_stream.print(in_file_contents)
   end
 end
--- spec/file_sender_spec.rb
+++ spec/file_sender_spec.rb
@@ -1,19 +1,28 @@
 require_relative "../file_sender.rb"
 
 RSpec.describe FileSender do
-  context "with a file containing 'abc'" do
-    let(:file_sender) { FileSender.new(file_sender_args) }
-    let(:file_sender_args) do
-      { in_file: in_file, in_stream: in_stream, out_stream: out_stream }
-    end
-    let(:out_stream) { StringIO.new }
-    let(:in_stream) { StringIO.new }
-    let(:in_file_contents) { "abc" }
-    let(:in_file) { StringIO.new(in_file_contents) }
+  let(:file_sender) { FileSender.new(file_sender_args) }
+  let(:file_sender_args) do
+    { in_file: in_file, in_stream: in_stream, out_stream: out_stream }
+  end
+  let(:out_stream) { StringIO.new }
+  let(:in_stream) { StringIO.new }
+  let(:in_file) { StringIO.new(in_file_contents) }
 
-    before { file_sender.call }
+  before { file_sender.call }
+
+  context "with a file containing 'abc'" do
+    let(:in_file_contents) { "abc" }
 
     it "sends 'abc'" do
+      expect(out_stream.string).to eq in_file_contents
+    end
+  end
+
+  context "with a file containing 'abcde'" do
+    let(:in_file_contents) { "abcde" }
+
+    it "sends 'abcde'" do
       expect(out_stream.string).to eq in_file_contents
     end
   end
