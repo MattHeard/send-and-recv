e2699021d2f505080c852506c99c3283a0beae13
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:18:30 +1300

    Extract local variables into let variables

--- spec/file_sender_spec.rb
+++ spec/file_sender_spec.rb
@@ -2,14 +2,18 @@ require_relative "../file_sender.rb"
 
 RSpec.describe FileSender do
   context "with a file containing 'abc'" do
+    let(:file_sender) { FileSender.new(file_sender_args) }
+    let(:file_sender_args) do
+      { in_file: in_file, in_stream: in_stream, out_stream: out_stream }
+    end
+    let(:out_stream) { StringIO.new }
+    let(:in_stream) { StringIO.new }
+    let(:in_file_contents) { "abc" }
+    let(:in_file) { StringIO.new(in_file_contents) }
+
+    before { file_sender.call }
+
     it "sends 'abc'" do
-      in_file_contents = "abc"
-      in_file = StringIO.new(in_file_contents)
-      in_stream = StringIO.new
-      out_stream = StringIO.new
-      args = { in_file: in_file, in_stream: in_stream, out_stream: out_stream }
-      file_sender = FileSender.new(args)
-      file_sender.call
       expect(out_stream.string).to eq in_file_contents
     end
   end
