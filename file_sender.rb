0067235065b961721dae9883561c448e28fba299
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:32:40 +1300

    Default in_file to input.txt and out_stream to STDOUT

--- file_sender.rb
+++ file_sender.rb
@@ -1,7 +1,11 @@
 class FileSender
+  IN_FILE_NAME = "input.txt"
+
   attr_reader :in_file, :out_stream
 
-  def initialize(args)
+  def initialize(args = {})
+    args = defaults.merge(args)
+
     @in_file = args[:in_file]
     @out_stream = args[:out_stream]
   end
@@ -10,5 +14,13 @@ class FileSender
     in_file_contents = in_file.string
 
     out_stream.print(in_file_contents)
+  end
+
+  private
+
+  def defaults
+    default_in_file = File.readlines(IN_FILE_NAME)
+
+    { :in_file => default_in_file, :out_stream => STDOUT }
   end
 end
