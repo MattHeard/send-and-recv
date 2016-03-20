4d04224f7f1050fb3976a58c00ea6cd04b2b8828
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:15:24 +1300

    Add out_stream to FileSender

--- file_sender.rb
+++ file_sender.rb
@@ -1,2 +1,9 @@
 class FileSender
+  def initialize(args)
+    @out_stream = args[:out_stream]
+  end
+
+  def call
+    @out_stream.print("abc")
+  end
 end
--- spec/file_sender_spec.rb
+++ spec/file_sender_spec.rb
@@ -1,4 +1,16 @@
 require_relative "../file_sender.rb"
 
 RSpec.describe FileSender do
+  context "with a file containing 'abc'" do
+    it "sends 'abc'" do
+      in_file_contents = "abc"
+      in_file = StringIO.new(in_file_contents)
+      in_stream = StringIO.new
+      out_stream = StringIO.new
+      args = { in_file: in_file, in_stream: in_stream, out_stream: out_stream }
+      file_sender = FileSender.new(args)
+      file_sender.call
+      expect(out_stream.string).to eq in_file_contents
+    end
+  end
 end
