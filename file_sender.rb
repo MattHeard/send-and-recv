c5b56b143f36464ed9f430e04521494fe4e551e6
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 10:24:04 +1300

    Send out_stream message instead of accessing variable

--- file_sender.rb
+++ file_sender.rb
@@ -1,9 +1,11 @@
 class FileSender
+  attr_reader :out_stream
+
   def initialize(args)
     @out_stream = args[:out_stream]
   end
 
   def call
-    @out_stream.print("abc")
+    out_stream.print("abc")
   end
 end
