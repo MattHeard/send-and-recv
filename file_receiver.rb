147fde7a660b5152989509364123a94fef4f6aae
Matt Heard <matt.heard@powershop.co.nz>
2016-03-21 15:23:37 +1300

    Match received digest against out file digest

--- file_receiver.rb
+++ file_receiver.rb
@@ -1,14 +1,56 @@
+require 'digest'
+
 class FileReceiver
-  attr_reader :in_stream, :out_file
+  DIGEST_NOT_MATCHING = "0"
+  DIGEST_MATCHING = "1"
+
+  attr_accessor :sent_digest
+  attr_reader :in_stream, :out_stream, :out_file
 
   def initialize(args = {})
     @in_stream = args[:in_stream]
+    @out_stream = args[:out_stream]
     @out_file = args[:out_file]
   end
 
   def call
-    in_stream_contents = in_stream.string
+    receive_file_digest
+    send_digest_match
 
+    update_out_file
+  end
+
+  private
+
+  def digest_matched?
+    sent_digest == (out_file_digest + "\n")
+  end
+
+  def out_file_digest
+    Digest::MD5.new.update(out_file_contents).hexdigest
+  end
+
+  def out_file_contents
+    out_file.string
+  end
+
+  def receive_file_digest
+    self.sent_digest = in_stream.gets
+  end
+
+  def send_digest_match
+    out_stream.puts digest_match_response
+  end
+
+  def digest_match_response
+    digest_matched? ? DIGEST_MATCHING : DIGEST_NOT_MATCHING
+  end
+
+  def update_out_file
     out_file.print(in_stream_contents)
+  end
+
+  def in_stream_contents
+    in_stream.string
   end
 end
--- spec/file_receiver_spec.rb
+++ spec/file_receiver_spec.rb
@@ -7,23 +7,69 @@ RSpec.describe FileReceiver do
   end
   let(:out_stream) { StringIO.new }
   let(:in_stream) { StringIO.new(in_stream_contents) }
-  let(:out_file) { StringIO.new }
+  let(:out_file) { StringIO.new(out_file_contents) }
+  let(:out_file_contents) { "" }
+  let(:expected_out_file_contents) { out_file_contents }
 
   before { file_receiver.call }
 
-  context "receiving 'abc'" do
-    let(:in_stream_contents) { "abc" }
+  context "when the out_file is empty" do
+    context "receiving the checksum of then contents of 'abc'" do
+      let(:in_stream_contents) { "900150983cd24fb0d6963f7d28e17f72\nabc" }
+      let(:expected_out_file_contents) { "abc" }
 
-    it "puts 'abc' in the output file" do
-      expect(out_file.string).to eq in_stream_contents
+      it "puts '0' in the out stream" do
+        expect(out_stream.string).to eq "0\n"
+      end
+
+      it "puts 'abc' in the output file" do
+        expect(out_file.string).to eq in_stream_contents
+      end
+    end
+
+    context "receiving the checksum of then contents of 'abcde'" do
+      let(:in_stream_contents) { "ab56b4d92b40713acc5af89985d4b786\nabcde" }
+      let(:expected_out_file_contents) { "abcde" }
+
+      it "puts '0' in the out stream" do
+        expect(out_stream.string).to eq "0\n"
+      end
+
+      it "puts 'abcde' in the output file" do
+        expect(out_file.string).to eq in_stream_contents
+      end
     end
   end
 
-  context "receiving 'abcde'" do
-    let(:in_stream_contents) { "abcde" }
+  context "when the out_file contains 'abc'" do
+    let(:out_file_contents) { "abc" }
 
-    it "puts 'abcde' in the output file" do
-      expect(out_file.string).to eq in_stream_contents
+    context "receiving only the checksum of 'abc'" do
+      let(:in_stream_contents) { "900150983cd24fb0d6963f7d28e17f72\n" }
+
+      it "puts '1' in the out stream" do
+        expect(out_stream.string).to eq "1\n"
+      end
+
+      it "leaves 'abc' in the output file" do
+        expect(out_file.string).to eq expected_out_file_contents
+      end
+    end
+  end
+
+  context "when the out_file contains 'abcde'" do
+    let(:out_file_contents) { "abcde" }
+
+    context "receiving only the checksum of 'abcde'" do
+      let(:in_stream_contents) { "ab56b4d92b40713acc5af89985d4b786\n" }
+
+      it "puts '1' in the out stream" do
+        expect(out_stream.string).to eq "1\n"
+      end
+
+      it "leaves 'abcde' in the output file" do
+        expect(out_file.string).to eq expected_out_file_contents
+      end
     end
   end
 end
